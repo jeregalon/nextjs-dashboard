390ab0830b9e567a90f4b946172881d5d71124fb
jeregalon <jeregalon@gmail.com>
2025-11-15 05:43:59 -0500

    using suspense to load components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,30 @@
-export default function DashboardPage () {
-  return <p>Dashboard Page</p>
+import { Suspense } from "react"
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data"
+import LatestInvoices from "../ui/dashboard/latest-invoices"
+import RevenueChart from "../ui/dashboard/revenue-chart"
+import { lusitana } from "../ui/fonts"
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons"
+
+export default async function DashboardPage () {  
+  /*Un componente que se utilice solo en el servidor
+  puede ser asíncrono*/
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  )
 }
