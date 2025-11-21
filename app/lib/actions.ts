1393a992e2a2907e7c400f7c6ebd803eecb17954
jeregalon <jeregalon@gmail.com>
2025-11-21 08:31:22 -0500

    editing and deleting invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -22,6 +22,11 @@ const CreateInvoiceSchema = CreateInvoiceFormSchema.omit({
   date: true
 })
 
+const UpdateInvoice = CreateInvoiceFormSchema.omit({ 
+  id: true, 
+  date: true 
+});
+
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoiceSchema.parse({
     customerId: formData.get('customerId'),
@@ -41,4 +46,28 @@ export async function createInvoice(formData: FormData) {
   
   /*Redireccionar al usuario a la página anterior*/
   redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
