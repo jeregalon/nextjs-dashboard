b54297849b5b63fefbfbbff986f68c0961371ee5
jeregalon <jeregalon@gmail.com>
2025-11-26 10:16:39 -0500

    form validation when editing invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,7 +29,7 @@ const CreateInvoiceSchema = CreateInvoiceFormSchema.omit({
 })
 
 const UpdateInvoice = CreateInvoiceFormSchema.omit({ 
-  id: true, 
+  id: true,
   date: true 
 });
 
@@ -88,13 +88,26 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices')
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(prevState: State, id: string, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+      values: {
+        customerId: String(formData.get('customerId') ?? ""),
+        amount: String(formData.get('amount') ?? ""),
+        status: String(formData.get('status') ?? ""),
+      }
+    };
+  }
  
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { updateInvoice } from '@/app/lib/actions';
+
+import { State, updateInvoice } from '@/app/lib/actions';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -9,6 +10,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,10 +19,21 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  
+  const initialState: State = {
+    message: null,
+    errors: {},
+    values: {}
+  };
+
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form 
+      action={formAction}
+      aria-describedby="missing-data-error"
+    >
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -62,9 +75,17 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount?.map(error => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
           </div>
         </div>
 
@@ -110,6 +131,11 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        <div id="missing-data-error" aria-live="polite" aria-atomic="true">
+          {state.message && (
+            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+          )}
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
